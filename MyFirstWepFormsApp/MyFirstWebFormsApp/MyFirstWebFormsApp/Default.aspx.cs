d1f7b15b39ab2bba260329a72577227b47fc0cef
japhlet <etyangemodojaphlet@yahoo.com>
2021-02-12 11:36:10 +0300

    "Added a redirect to contact button in the master page"

--- MyFirstWepFormsApp/MyFirstWebFormsApp/MyFirstWebFormsApp/Default.aspx.cs
+++ MyFirstWepFormsApp/MyFirstWebFormsApp/MyFirstWebFormsApp/Default.aspx.cs
@@ -13,5 +13,10 @@ namespace MyFirstWebFormsApp
         {
 
         }
+
+        protected void btnRedirect_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Contact");
+        }
     }
 }
